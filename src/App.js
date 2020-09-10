f1c5e022af8b098543b2e85ca8e60640a9517494
Lianghaha <jialiang0601@gmail.com>
2020-09-09 23:38:26 -0400

    -mwrote some front page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,57 @@
 import React from 'react';
 import './App.css';
+import { VscFoldDown } from 'react-icons/vsc'
 
 function App() {
   return (
-    <div className="App">
-      Hello World 123
-    </div>
-  );
+     <div className="App">
+
+        <div className="Navbar">
+           <ul className="NavLeft">
+              <li>
+                 <a href="google.com">The Prohets</a>
+              </li>
+              <li>
+                 <a href="google.com">Prohets</a>
+              </li>
+              <li>
+                 <a href="google.com">Predictions</a>
+              </li>
+           </ul>
+           <ul className="NavRight">
+              <li>
+                 <a href="google.com">Sign In</a>
+              </li>
+              <li>
+                 <a href="google.com">Login</a>
+              </li>
+           </ul>
+        </div>
+        <div className="clear-float-div-common"></div>
+
+
+
+
+        <div className="Cover">
+           <div className="CoverText">
+              <h1>Find The Prohets</h1>
+              <p>
+                 asdasdajsldkajskldjaskldjaklsjdlaksjdaklsjdlaksjdklajsdlkajsdklajsdklajsdklajskldajsldkjaskldjalskdjalksdjalksjdlaksjdlaksjdklajsdl
+              </p>
+           </div>
+           <div className="Icon">
+              <VscFoldDown color="white" size="2em" />
+           </div>
+        </div>
+        <div className="clear-float-div-common"></div>
+
+
+
+        <div className="ProphetsList">
+          <h2>Top Prophets</h2>
+        </div>
+     </div>
+  )
 }
 
 export default App;
