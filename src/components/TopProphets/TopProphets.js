be8b5cc26784bdfb256787f0d4a6cefc47dedae6
Lianghaha <jialiang0601@gmail.com>
2020-09-15 21:49:32 -0400

    added buttons for Top Prophets List and add some responsiveness

--- src/components/TopProphets/TopProphets.js
+++ src/components/TopProphets/TopProphets.js
@@ -1,8 +1,12 @@
-import React from "react"
+import React, {useRef} from "react"
 import "./TopProphets.css"
 import Prophet from "./Prophet/Prophet"
 //Antd
 import { Carousel } from "antd"
+//Icons
+import { GoChevronRight, GoChevronLeft } from "react-icons/go"
+//Button
+import Button from "@material-ui/core/Button"
 
 window.addEventListener("load", addID())
 
@@ -14,14 +18,16 @@ function addID() {
    console.log(element[0])
 }
 
+
+
 //Antd
 const settings = {
    arrows: false,
    dots: false,
-   infinite: false,
-   speed: 500,
+   infinite: true,
+   speed: 1200,
    slidesToShow: 6,
-   slidesToScroll: 3,
+   slidesToScroll: 6,
    initialSlide: 0,
    draggable: true,
    responsive: [
@@ -29,28 +35,28 @@ const settings = {
          breakpoint: 2200,
          settings: {
             slidesToShow: 5,
-            slidesToScroll: 3,
+            slidesToScroll: 5,
          },
       },
       {
          breakpoint: 1750,
          settings: {
             slidesToShow: 4,
-            slidesToScroll: 2,
+            slidesToScroll: 4,
          },
       },
       {
          breakpoint: 1350,
          settings: {
             slidesToShow: 3,
-            slidesToScroll: 1,
+            slidesToScroll: 3,
          },
       },
       {
          breakpoint: 1000,
          settings: {
             slidesToShow: 2,
-            slidesToScroll: 1,
+            slidesToScroll: 2,
          },
       },
       {
@@ -65,11 +71,32 @@ const settings = {
 
 
 function TopProphets(props) {
+   const carouselRef = useRef()
+   function CarouselNext(){
+      carouselRef.current.next();
+   }
+   function CarouselPrev() {
+      carouselRef.current.prev()
+   }
+
    return (
-      <div id="123" className="TopProphets">
-         <h2>Top Prophets</h2>
+      <div className="TopProphets">
+         <div className="TitleAndButtons">
+            <h2>Top Prophets</h2>
+            <div className="TitleButtons">
+               <Button variant="outlined">
+                  ALL
+               </Button>
+               <Button onClick={CarouselPrev} variant="outlined">
+                  <GoChevronLeft color="white" size="1.8em" />
+               </Button>
+               <Button onClick={CarouselNext} variant="outlined">
+                  <GoChevronRight color="white" size="1.8em" />
+               </Button>
+            </div>
+         </div>
          <div className="ProphetsList">
-            <Carousel {...settings}>
+            <Carousel ref={carouselRef} {...settings}>
                {props.data.map((data) => {
                   return (
                      <div className="Card">
